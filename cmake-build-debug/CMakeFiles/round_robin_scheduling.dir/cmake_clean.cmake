75dde510caa64c0b073d02eff0885923e0a0f65b
Hussein-Mourad <husseinmouradkassem9901@gmail.com>
2021-05-11 16:49:02 +0200

    init

---
+++ cmake-build-debug/CMakeFiles/round_robin_scheduling.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/round_robin_scheduling.dir/Queue.c.o"
+  "round_robin_scheduling"
+  "round_robin_scheduling.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/round_robin_scheduling.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
