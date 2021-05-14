6c85853d3503a1f180d8b77f8bb48a54e68c6646
Hussein-Mourad <husseinmouradkassem9901@gmail.com>
2021-05-14 02:48:45 +0200

    implemented queue

--- Queue.c
+++ Queue.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 /*
  * Process: Each process has a name and has an execution time.
  *          We keep track of how long the process still needs
  *          to run on the processor. "remaining_time" is the
  *          remaining execution time of the process.
  */
-typedef struct
-{
+typedef struct {
     char *name;
     int starting_time;
     int remaining_time;
@@ -16,76 +16,139 @@ typedef struct
 /*
  *
  */
-typedef struct Node
-{
+typedef struct Node {
     Process data;
-    struct Node* next;
+    struct Node *next;
 } Node;
-/*
- *
- */
-typedef struct
-{
-    /* TODO: ADD YOUR CODE HERE */
-} Queue;
-/*
- *
- */
-Queue* init()
-{
-    /* TODO: ADD YOUR CODE HERE */
-}
-/*
- *
- */
-int isEmpty(Queue *q)
-{
-    /* TODO: ADD YOUR CODE HERE */
 
+Node *newNode(Process p) {
+    Node *n = malloc(sizeof(Node));
+    n->data = p;
+    n->next = NULL;
+    return n;
 }
+
 /*
  *
  */
-Process dequeue(Queue *q)
-{
-    /* TODO: ADD YOUR CODE HERE */
-}
+typedef struct {
+    Node *head;
+    Node *tail;
+} Queue;
+
 /*
  *
  */
-void enqueue(Queue *q, Process x)
-{
-    /* TODO: ADD YOUR CODE HERE */
+Queue *init() {
+    Queue *q = malloc(sizeof(Queue));
+    q->head = NULL;
+    q->tail = NULL;
+    return q;
 }
+
 /*
  *
  */
-void destroy(Queue *q)
-{
-    /* TODO: ADD YOUR CODE HERE */
+int isEmpty(Queue *q) {
+    return q->head == NULL;
 }
+
+/*
+ *
+ */
+Process dequeue(Queue *q) {
+    if (q->head) {
+        Process result;
+        Node *temp = q->head;
+        result = temp->data;
+        q->head = temp->next;
+        free(temp);
+        if (q->head == NULL)
+            q->tail = NULL;
+        return result;
+    }
+}
+
+/*
+ *
+ */
+void enqueue(Queue *q, Process x) {
+    Node *n = newNode(x);
+    if (q->head == NULL)
+        q->head = q->tail = n;
+    else {
+        q->tail->next = n;
+        q->tail = n;
+    }
+}
+
+/*
+ *
+ */
+void destroy(Queue *q) {
+    while (!isEmpty(q)) {
+        dequeue(q);
+    }
+}
+
+Process *getProcesses(FILE *f, int watchTime) {
+    char process_name[2];
+    int starting_time;
+    int remaining_time;
+    Process processes[watchTime];
+
+    int i = 0;
+    while (!feof(f)) {
+        // reads the processes
+        fscanf(f, "%s %d %d", process_name, &starting_time, &remaining_time);
+        Process process = {process_name, starting_time, remaining_time};
+        processes[i++] = process;
+    }
+    return processes;
+}
+
+int getWatchTime(FILE *f) {
+    int watch_time;
+    char buffer[10];
+    // gets watching time slots
+    fscanf(f, "%s %s %s %s %d", buffer, buffer, buffer, buffer, &watch_time);
+    return watch_time;
+}
+
 /*
  * RoundRobin Scheduling
  */
-void RoundRobin(char* filename)
-{
-    /* TODO: ADD YOUR CODE HERE */
+void RoundRobin(char *filename) {
+    FILE *f = fopen(filename, "r");
+    Queue *queue = init();
+    int watch_time = getWatchTime(f);
+    Process *processes = getProcesses(f, watch_time);
+    int number_of_processes = sizeof processes / sizeof processes[0];
+
+    for (int i = 0; i < watch_time; i++) {
+        Process process = processes[i % number_of_processes];
+        if (process.starting_time == i)
+            enqueue(queue, process);
+        Process current_process = dequeue(queue);
+        current_process.remaining_time = current_process.remaining_time - 1;
+        char *process_id = isEmpty(queue) ? "idle" : current_process.name;
+        char *message = current_process.remaining_time == 0 ? strcat(process_id, " aborts") : "";
+        printf("%s (%d-->%d) %s\n", process_id, i, i + 1, message);
+    }
 }
+
 /*
  *
  */
-int main()
-{
+int main() {
     char filename[261];
     puts("Enter file name or Ctrl+Z to exit:");
     puts("----------------------------------");
-    while(fgets(filename, 260, stdin) != NULL)
-    {
-        filename[strlen(filename)-1]='\0';
-        if(fopen(filename,"r"))
+    while (fgets(filename, 260, stdin) != NULL) {
+        filename[strlen(filename) - 1] = '\0';
+        if (fopen(filename, "r"))
             RoundRobin(filename);
-        else
-        {
+        else {
             puts("File Not Found!");
             puts("----------------------------------");
         }
--- cmake-build-debug/CMakeFiles/round_robin_scheduling.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/round_robin_scheduling.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/home/hussein/CLionProjects/round-robin-scheduling/Queue.c" "/home/hussein/CLionProjects/round-robin-scheduling/cmake-build-debug/CMakeFiles/round_robin_scheduling.dir/Queue.c.o"
+  "/mnt/d/CLionProjects/round-robin-scheduling/Queue.c" "/mnt/d/CLionProjects/round-robin-scheduling/cmake-build-debug/CMakeFiles/round_robin_scheduling.dir/Queue.c.o"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
